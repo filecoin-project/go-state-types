db9a3a49bd2dbc5e93a335b210fa35a8d7994039
ZenGround0 <ZenGround0@users.noreply.github.com>
2020-10-13 11:43:14 -0400

    Prep for upcoming upgrades

--- network/version.go
+++ network/version.go
@@ -11,8 +11,9 @@ const (
 	Version1                 // breeze    (specs-actors v0.9.7)
 	Version2                 // smoke     (specs-actors v0.9.8)
 	Version3                 // ignition  (specs-actors v0.9.11)
-	Version4                 // actors v2 (specs-actors v2.0.x (future))
-	Version5                 // sometime?
+	Version4                 // actors v2 (specs-actors v2.0.3)
+	Version5                 // tape      (specs-actors v2.1.0)
+	Version6                 // upcoming
 
 	// VersionMax is the maximum version number
 	VersionMax = Version(math.MaxUint32)
