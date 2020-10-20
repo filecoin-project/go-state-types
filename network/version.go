e98724316b6376ff5d1ca2413f645adc0863a88a
acruikshank <acruikshank@example.com>
2020-10-20 17:30:54 -0400

    whitespace

--- network/version.go
+++ network/version.go
@@ -14,9 +14,9 @@ const (
 	Version4                 // actors v2 (specs-actors v2.0.3)
 	Version5                 // tape      (specs-actors v2.1.0)
 	Version6                 // pending   (specs-actors v2.2.0)
-        Version7                 // upcoming
-        Version8                 // reserved
-        Version9                 // reserved
+	Version7                 // upcoming
+	Version8                 // reserved
+	Version9                 // reserved
 
 	// VersionMax is the maximum version number
 	VersionMax = Version(math.MaxUint32)
