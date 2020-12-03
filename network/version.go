7cab7f0d4bfb9dfa9c94d64fd51c61cf28513df2
Alex <445306+anorth@users.noreply.github.com>
2020-12-03 13:23:37 +1100

    Describe versions 7 and 8, add 10 (#20)

--- network/version.go
+++ network/version.go
@@ -14,9 +14,10 @@ const (
 	Version4                 // actors v2 (specs-actors v2.0.3)
 	Version5                 // tape      (specs-actors v2.1.0)
 	Version6                 // kumquat   (specs-actors v2.2.0)
-	Version7                 // upcoming
-	Version8                 // reserved
+	Version7                 // calico    (specs-actors v2.3.2)
+	Version8                 // persian   (post-2.3.2 behaviour transition)
 	Version9                 // reserved
+	Version10                // reserved
 
 	// VersionMax is the maximum version number
 	VersionMax = Version(math.MaxUint32)
