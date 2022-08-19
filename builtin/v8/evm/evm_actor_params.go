c81f673ea1944d10eb673eeebb00f579de292e32
vyzo <vyzo@hackzen.org>
2022-08-19 20:55:32 +0300

    remove InvokeReturn, type doesn't exist

--- builtin/v8/evm/evm_actor_params.go
+++ builtin/v8/evm/evm_actor_params.go
@@ -1,12 +1,5 @@
 package evm
 
-import (
-	"fmt"
-	"io"
-
-	cbg "github.com/whyrusleeping/cbor-gen"
-)
-
 type ConstructorParams struct {
 	Bytecode  []byte
 	InputData []byte
@@ -14,36 +7,4 @@ type ConstructorParams struct {
 
 type InvokeParams struct {
 	InputData []byte
-}
-
-type InvokeReturn struct {
-	OutputData []byte
-}
-
-func (t *InvokeReturn) UnmarshalCBOR(r io.Reader) error {
-	*t = InvokeReturn{}
-
-	br := cbg.GetPeeker(r)
-	scratch := make([]byte, 8)
-
-	maj, extra, err := cbg.CborReadHeaderBuf(br, scratch)
-	if err != nil {
-		return err
-	}
-
-	if extra > cbg.ByteArrayMaxLen {
-		return fmt.Errorf("t.InputData: byte array too large (%d)", extra)
-	}
-	if maj != cbg.MajByteString {
-		return fmt.Errorf("expected byte array")
-	}
-
-	if extra > 0 {
-		t.OutputData = make([]uint8, extra)
-	}
-
-	if _, err := io.ReadFull(br, t.OutputData[:]); err != nil {
-		return err
-	}
-	return nil
 }
