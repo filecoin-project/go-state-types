5f435c6efbefc52df0c345c41a26f13f9bedf1b4
Geoff Stuart <geoff.vball@gmail.com>
2022-10-12 12:43:41 -0400

    make datacap methods a map instead of array (#94)

--- builtin/v9/datacap/methods.go
+++ builtin/v9/datacap/methods.go
@@ -5,7 +5,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-var Methods = []interface{}{
+var Methods = map[uint64]interface{}{
 	1: *new(func(interface{}, *address.Address) *abi.EmptyValue), // Constructor
 	2: *new(func(interface{}, *MintParams) *MintReturn),          // Mint
 	3: *new(func(interface{}, *DestroyParams) *BurnReturn),       // Destroy
