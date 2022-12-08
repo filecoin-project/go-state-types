f0b529b2b470f93bc8752cb1f84c358172353392
Shrenuj Bansal <shrenuj.bansal@protocol.ai>
2022-12-08 18:01:14 -0500

    Add a function to generate frc42 method number

--- builtin/methods.go
+++ builtin/methods.go
@@ -1,7 +1,11 @@
 package builtin
 
 import (
+	"encoding/binary"
 	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/minio/blake2b-simd"
+	"golang.org/x/xerrors"
+	"unicode"
 )
 
 const (
@@ -161,3 +165,45 @@ var MethodsEAM = struct {
 	Create      abi.MethodNum
 	Create2     abi.MethodNum
 }{MethodConstructor, 2, 3}
+
+// Generates a standard FRC-42 compliant method number
+// Reference: https://github.com/filecoin-project/FIPs/blob/master/FRCs/frc-0042.md
+func GenerateMethodNum(name string) (abi.MethodNum, error) {
+	err := validateMethodName(name)
+	if err != nil {
+		return 0, err
+	}
+
+	if name == "Constructor" {
+		return MethodConstructor, nil
+	}
+
+	digest := blake2b.Sum512([]byte("1|" + name))
+
+	for i := 0; i < 64; i += 4 {
+		methodId := binary.BigEndian.Uint32(digest[i : i+4])
+		if methodId >= (1 << 24) {
+			return abi.MethodNum(methodId), nil
+		}
+	}
+
+	return abi.MethodNum(0), xerrors.Errorf("Could not generate method num from method name :", name)
+}
+
+func validateMethodName(name string) error {
+	if name == "" {
+		return xerrors.Errorf("empty name string")
+	}
+
+	if !unicode.IsUpper(rune(name[0])) {
+		return xerrors.Errorf("Method name first letter must be uppercase, method name: ", name)
+	}
+
+	for _, c := range name {
+		if !(unicode.IsLetter(c) || unicode.IsDigit(c) || c == '_') {
+			return xerrors.Errorf("method name has illegal characters, method name: ", name)
+		}
+	}
+
+	return nil
+}
