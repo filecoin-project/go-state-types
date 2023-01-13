42b5c6b86d2ee7e106ee1ce051b60ac3e1865aee
vyzo <vyzo@hackzen.org>
2023-01-13 22:08:17 +0200

    use CborBytes instead of CreateExternalParams

--- builtin/v10/eam/eam_types.go
+++ builtin/v10/eam/eam_types.go
@@ -1,12 +1,7 @@
 package eam
 
 import (
-	"fmt"
-	"io"
-
 	"github.com/filecoin-project/go-address"
-
-	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 type CreateParams struct {
@@ -19,11 +14,6 @@ type Create2Params struct {
 	Salt     [32]byte
 }
 
-// this is transparent
-type CreateExternalParams struct {
-	Initcode []byte
-}
-
 type Return struct {
 	ActorID       uint64
 	RobustAddress *address.Address
@@ -33,22 +23,3 @@ type Return struct {
 type CreateReturn Return
 type Create2Return Return
 type Create3Return Return
-
-// cbg doesn't support transparent structs  so we do this by hand
-func (t *CreateExternalParams) MarshalCBOR(w io.Writer) error {
-	if len(t.Initcode) > cbg.ByteArrayMaxLen {
-		return fmt.Errorf("Byte array in field t.Initcode was too long")
-	}
-
-	scratch := make([]byte, 9)
-
-	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.Initcode))); err != nil {
-		return err
-	}
-
-	if _, err := w.Write(t.Initcode[:]); err != nil {
-		return err
-	}
-
-	return nil
-}
