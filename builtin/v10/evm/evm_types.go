43d260f6617e7161b0a16c133cf03672adf27869
Steven Allen <steven@stebalien.com>
2023-02-10 14:31:50 -0800

    fix: EVM::GetBytecode now returns an Option (#160)
    Unfortunately, this requires a manual CBOR implementation.

--- builtin/v10/evm/evm_types.go
+++ builtin/v10/evm/evm_types.go
@@ -1,9 +1,13 @@
 package evm
 
 import (
+	"io"
+
 	"github.com/ipfs/go-cid"
+	xerrors "golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
+	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 type ConstructorParams struct {
@@ -22,4 +26,44 @@ type DelegateCallParams struct {
 	Input  []byte
 	Caller [20]byte
 	Value  abi.TokenAmount
+}
+
+type GetBytecodeReturn struct {
+	Cid *cid.Cid
+}
+
+func (bc *GetBytecodeReturn) UnmarshalCBOR(r io.Reader) error {
+	if bc == nil {
+		return xerrors.Errorf("cannot unmarshal into nil pointer")
+	}
+
+	br := cbg.GetPeeker(r)
+
+	// reset fields
+	bc.Cid = nil
+
+	// Check if it's null
+	if byte, err := br.ReadByte(); err != nil {
+		return err
+	} else if byte == cbg.CborNull[0] {
+		return nil
+	} else if err := br.UnreadByte(); err != nil {
+		return err
+	}
+
+	c, err := cbg.ReadCid(br)
+	if err != nil {
+		return xerrors.Errorf("failed to read cid field t.OldPtr: %w", err)
+	}
+	bc.Cid = &c
+	return nil
+}
+
+func (bc *GetBytecodeReturn) MarshalCBOR(w io.Writer) error {
+	if bc == nil || bc.Cid == nil {
+		_, err := w.Write(cbg.CborNull)
+		return err
+	}
+
+	return cbg.WriteCid(w, *bc.Cid)
 }
