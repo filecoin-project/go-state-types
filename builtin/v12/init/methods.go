56e2d34a367d0a0eb5dcf0bdd3509138b7c02e81
Raúl Kripalani <raul@protocol.ai>
2023-02-13 15:29:20 +0000

    pull in GetBytecode fixes + more from master.

--- builtin/v12/init/methods.go
+++ builtin/v12/init/methods.go
@@ -6,9 +6,8 @@ import (
 )
 
 var Methods = map[abi.MethodNum]builtin.MethodMeta{
-	1:                                        {"Constructor", *new(func(*ConstructorParams) *abi.EmptyValue)}, // Constructor
-	2:                                        {"Exec", *new(func(*ExecParams) *ExecReturn)},                   // Exec
-	builtin.MustGenerateFRCMethodNum("Exec"): {"ExecExported", *new(func(*ExecParams) *ExecReturn)},           // ExecExported
+	1: {"Constructor", *new(func(*ConstructorParams) *abi.EmptyValue)}, // Constructor
+	2: {"Exec", *new(func(*ExecParams) *ExecReturn)},                   // Exec
 	// TODO Are we exporting Exec4
 	3: {"Exec4", *new(func(*Exec4Params) *ExecReturn)}, // Exec4
 }
