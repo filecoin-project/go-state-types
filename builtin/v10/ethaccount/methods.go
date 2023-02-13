25fbf188ce8daf73f7f0a58c8b53e26a00e4b7fe
snissn <snissn@gmail.com>
2023-02-13 13:00:56 -0700

    remove UniversalReceiverHook from go-state-types to match rust (#163)

--- builtin/v10/ethaccount/methods.go
+++ builtin/v10/ethaccount/methods.go
@@ -8,5 +8,4 @@ import (
 var Methods = map[abi.MethodNum]builtin.MethodMeta{
 	1: {"Constructor", *new(func(value *abi.EmptyValue) *abi.EmptyValue)},
 	builtin.MustGenerateFRCMethodNum("AuthenticateMessage"): {"AuthenticateMessage", *new(func(*AuthenticateMessageParams) *abi.EmptyValue)},  // AuthenticateMessage
-	builtin.MustGenerateFRCMethodNum("Receive"):             {"UniversalReceiverHook", *new(func(*abi.CborBytesTransparent) *abi.EmptyValue)}, // UniversalReceiverHook
 }
