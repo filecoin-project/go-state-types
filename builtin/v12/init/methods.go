3fdaef93398f0d04c9ca02b80aeec5b75f4c87f3
Raúl Kripalani <raul@protocol.ai>
2023-02-13 15:38:10 +0000

    drop TODO.

--- builtin/v12/init/methods.go
+++ builtin/v12/init/methods.go
@@ -8,6 +8,5 @@ import (
 var Methods = map[abi.MethodNum]builtin.MethodMeta{
 	1: {"Constructor", *new(func(*ConstructorParams) *abi.EmptyValue)}, // Constructor
 	2: {"Exec", *new(func(*ExecParams) *ExecReturn)},                   // Exec
-	// TODO Are we exporting Exec4
-	3: {"Exec4", *new(func(*Exec4Params) *ExecReturn)}, // Exec4
+	3: {"Exec4", *new(func(*Exec4Params) *ExecReturn)},                 // Exec4
 }
