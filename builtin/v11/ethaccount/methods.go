2b8f4e39028fe86bca818fcdcbfec5176d8802f6
Aayush Rajasekaran <arajasek94@gmail.com>
2023-02-20 12:30:43 -0500

    feat: add v11 types (#170)

---
+++ builtin/v11/ethaccount/methods.go
@@ -0,0 +1,11 @@
+package ethaccount
+
+import (
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/builtin"
+)
+
+var Methods = map[abi.MethodNum]builtin.MethodMeta{
+	1: {"Constructor", *new(func(value *abi.EmptyValue) *abi.EmptyValue)},
+	builtin.MustGenerateFRCMethodNum("AuthenticateMessage"): {"AuthenticateMessage", *new(func(*AuthenticateMessageParams) *abi.EmptyValue)}, // AuthenticateMessage
+}
