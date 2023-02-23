c67e35ae657002d419fd5a6c35eab038ce2b811d
Aayush <arajasek94@gmail.com>
2023-02-23 15:10:04 -0500

    feat: incorporate changes to AuthenticateMessage

--- builtin/v10/account/methods.go
+++ builtin/v10/account/methods.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"github.com/filecoin-project/go-address"
+	typegen "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/builtin"
@@ -10,5 +11,5 @@ import (
 var Methods = map[abi.MethodNum]builtin.MethodMeta{
 	1: {"Constructor", *new(func(*address.Address) *abi.EmptyValue)},   // Constructor
 	2: {"PubkeyAddress", *new(func(*abi.EmptyValue) *address.Address)}, // PubkeyAddress
-	builtin.MustGenerateFRCMethodNum("AuthenticateMessage"): {"AuthenticateMessage", *new(func(*AuthenticateMessageParams) *abi.EmptyValue)},  // AuthenticateMessage
+	builtin.MustGenerateFRCMethodNum("AuthenticateMessage"): {"AuthenticateMessage", *new(func(*AuthenticateMessageParams) *typegen.CborBool)}, // AuthenticateMessage
 }
