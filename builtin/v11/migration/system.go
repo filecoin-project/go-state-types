a60a9ac10c4d52be5b51b310bb1a00ea2c3c8e9a
Hubert Bugaj <hubert@chainsafe.io>
2023-05-12 11:18:34 +0200

    fix system actor typo in migration

--- builtin/v11/migration/system.go
+++ builtin/v11/migration/system.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/migration"
 
-	system10 "github.com/filecoin-project/go-state-types/builtin/v10/system"
+	system11 "github.com/filecoin-project/go-state-types/builtin/v11/system"
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
@@ -23,7 +23,7 @@ func (m systemActorMigrator) MigratedCodeCID() cid.Cid {
 
 func (m systemActorMigrator) MigrateState(ctx context.Context, store cbor.IpldStore, in migration.ActorMigrationInput) (*migration.ActorMigrationResult, error) {
 	// The ManifestData itself is already in the blockstore
-	state := system10.State{BuiltinActors: m.ManifestData}
+	state := system11.State{BuiltinActors: m.ManifestData}
 	stateHead, err := store.Put(ctx, &state)
 	if err != nil {
 		return nil, err
