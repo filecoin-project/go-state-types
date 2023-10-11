c7e971e2d48b6b473c899f7511ae414c988204b1
Jie Hou <jie.hou@protocol.ai>
2023-10-11 13:30:33 -0700

    Clean up

--- builtin/v12/miner/monies_test.go
+++ builtin/v12/miner/monies_test.go
@@ -8,38 +8,18 @@ import (
 	"testing"
 )
 
-func TestInitialPledgeForPower_NetworkQAPGreaterThanNetworkBaseline(t *testing.T) {
-	pledge := initialConsensusPledge(
-		abi.NewStoragePower(5),
-		abi.NewStoragePower(1000),
-		smoothing.NewEstimate(big.NewInt(1500), big.NewInt(0)),
-		abi.NewTokenAmount(10000))
-
-	assert.Equal(t, big.NewInt(10), pledge)
-}
-
 func TestInitialPledgeForPower2_NetworkQAPGreaterThanNetworkBaseline(t *testing.T) {
-	pledge := initialConsensusPledge2(
+	pledge := initialConsensusPledge(
 		abi.NewStoragePower(5),
 		abi.NewStoragePower(1000),
 		smoothing.NewEstimate(big.NewInt(1500), big.NewInt(0)),
 		abi.NewTokenAmount(10000))
 
 	assert.Equal(t, big.NewInt(10), pledge)
-}
-
-func TestInitialPledgeForPower_NetworkQAPLessThanNetworkBaseline(t *testing.T) {
-	pledge := initialConsensusPledge(
-		abi.NewStoragePower(5),
-		abi.NewStoragePower(1000),
-		smoothing.NewEstimate(big.NewInt(500), big.NewInt(0)),
-		abi.NewTokenAmount(10000))
-
-	assert.Equal(t, big.NewInt(15), pledge)
 }
 
 func TestInitialPledgeForPower2_NetworkQAPLessThanNetworkBaseline(t *testing.T) {
-	pledge := initialConsensusPledge2(
+	pledge := initialConsensusPledge(
 		abi.NewStoragePower(5),
 		abi.NewStoragePower(1000),
 		smoothing.NewEstimate(big.NewInt(500), big.NewInt(0)),
