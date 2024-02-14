9820cd599b9c6d18ad59ef799c275c40c460a9e9
Steven Allen <steven@stebalien.com>
2024-02-13 21:09:34 -0800

    feat: add names for "common" errors

--- exitcode/reserved.go
+++ exitcode/reserved.go
@@ -66,22 +66,3 @@ const (
 // The initial range of exit codes is reserved for system errors.
 // Actors may define codes starting with this one.
 const FirstActorErrorCode = ExitCode(16)
-
-var names = map[ExitCode]string{
-	Ok:                       "Ok",
-	SysErrSenderInvalid:      "SysErrSenderInvalid",
-	SysErrSenderStateInvalid: "SysErrSenderStateInvalid",
-	SysErrIllegalInstruction: "SysErrIllegalInstruction",
-	SysErrInvalidReceiver:    "SysErrInvalidReceiver",
-	SysErrInsufficientFunds:  "SysErrInsufficientFunds",
-	SysErrOutOfGas:           "SysErrOutOfGas",
-	SysErrIllegalExitCode:    "SysErrIllegalExitCode",
-	SysErrFatal:              "SysFatal",
-	SysErrMissingReturn:      "SysErrMissingReturn",
-	SysErrReserved1:          "SysErrReserved1",
-	SysErrReserved2:          "SysErrReserved2",
-	SysErrReserved3:          "SysErrReserved3",
-	SysErrReserved4:          "SysErrReserved4",
-	SysErrReserved5:          "SysErrReserved5",
-	SysErrReserved6:          "SysErrReserved6",
-}
