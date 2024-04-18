bc7aa961e068aad210bade08d0aa30df20927e5a
zenground0 <ZenGround0@users.noreply.github.com>
2024-04-18 01:13:50 -0500

    Migrate f090 to safety

--- builtin/v14/migration/top.go
+++ builtin/v14/migration/top.go
@@ -6,12 +6,14 @@ import (
 	adt14 "github.com/filecoin-project/go-state-types/builtin/v14/util/adt"
 
 	system13 "github.com/filecoin-project/go-state-types/builtin/v13/system"
+	account14 "github.com/filecoin-project/go-state-types/builtin/v14/account"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/builtin"
 	"github.com/filecoin-project/go-state-types/manifest"
 	"github.com/filecoin-project/go-state-types/migration"
 
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
@@ -78,7 +80,6 @@ func MigrateStateTree(ctx context.Context, store cbor.IpldStore, newManifestCID 
 	// migrations that migrate both code and state, override entries in `migrations`
 
 	// The System Actor
-
 	newSystemCodeCID, ok := newManifest.Get(manifest.SystemKey)
 	if !ok {
 		return cid.Undef, xerrors.Errorf("code cid for system actor not found in new manifest")
@@ -94,6 +95,45 @@ func MigrateStateTree(ctx context.Context, store cbor.IpldStore, newManifestCID 
 	if err != nil {
 		return cid.Undef, xerrors.Errorf("failed to run migration: %w", err)
 	}
+
+	// FIP 0085.  f090 multisig => unkeyed account actor
+	// Account state now points to id address requiring upgrade to release funds
+	// TODO move to small file
+	// 1. return error and new f090
+	// 2. pass in actors in in order to get f090 to construct new one
+	// 3. pass in actors out so we can flush it (maybe flush outside of fn? but I think we match other migration patterns by flushing in function? )
+	f090ID, err := address.NewIDAddress(90)
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("failed to construct f090 id addr", err)
+	}
+	f090OldAct, found, err := actorsOut.GetActorV5(f090ID)
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("failed to get old f090 actor", err)
+	}
+	if !found {
+		return cid.Undef, xerrors.Errorf("failed to find old f090 actor")
+	}
+	f090NewSt := account14.State{Address: f090ID} // State points to ID addr
+	h, err := store.Put(ctx, f090NewSt)
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("failed to write new f090 state")
+	}
+
+	newAccountCodeCID, ok := newManifest.Get(manifest.AccountKey)
+	if !ok {
+		return cid.Undef, xerrors.Errorf("invalid manifest missing account code cid")
+	}
+
+	actorsOut.SetActorV5(f090ID, &builtin.ActorV5{
+		// unchanged
+		CallSeqNum: f090OldAct.CallSeqNum,
+		Balance:    f090OldAct.Balance,
+		Address:    f090OldAct.Address,
+
+		// changed
+		Code: newAccountCodeCID,
+		Head: h,
+	})
 
 	outCid, err := actorsOut.Flush()
 	if err != nil {
