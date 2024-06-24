ac405c34beb04eaed44f405714a69e121c4a0d5c
Jiaying Wang <42981373+jennijuju@users.noreply.github.com>
2024-06-24 19:18:45 -0400

    land all nv23 changes in master  (#277)
    * Migrate f090 to safety (#259)
    
    * Migrate f090 to safety
    
    * Cleanup
    
    * Lint
    
    ---------
    
    Co-authored-by: zenground0 <ZenGround0@users.noreply.github.com>
    
    * fix: propagate errors on migration (#264)
    
    * fix: use adtStore instead of plain Store to write new state (#265)
    
    * fix: put a pointer to the store for marshalability (#266)
    
    ---------
    
    Co-authored-by: ZenGround0 <5515260+ZenGround0@users.noreply.github.com>
    Co-authored-by: zenground0 <ZenGround0@users.noreply.github.com>
    Co-authored-by: Rod Vagg <rod@vagg.org>

--- builtin/v14/migration/top.go
+++ builtin/v14/migration/top.go
@@ -6,12 +6,14 @@ import (
 	adt14 "github.com/filecoin-project/go-state-types/builtin/v14/util/adt"
 
 	system13 "github.com/filecoin-project/go-state-types/builtin/v13/system"
+	account14 "github.com/filecoin-project/go-state-types/builtin/v14/account"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/builtin"
 	"github.com/filecoin-project/go-state-types/manifest"
 	"github.com/filecoin-project/go-state-types/migration"
 
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
@@ -78,7 +80,6 @@ func MigrateStateTree(ctx context.Context, store cbor.IpldStore, newManifestCID 
 	// migrations that migrate both code and state, override entries in `migrations`
 
 	// The System Actor
-
 	newSystemCodeCID, ok := newManifest.Get(manifest.SystemKey)
 	if !ok {
 		return cid.Undef, xerrors.Errorf("code cid for system actor not found in new manifest")
@@ -93,6 +94,46 @@ func MigrateStateTree(ctx context.Context, store cbor.IpldStore, newManifestCID 
 	actorsOut, err := migration.RunMigration(ctx, cfg, cache, store, log, actorsIn, migrations)
 	if err != nil {
 		return cid.Undef, xerrors.Errorf("failed to run migration: %w", err)
+	}
+
+	// FIP 0085.  f090 multisig => unkeyed account actor
+	// Account state now points to id address requiring upgrade to release funds
+	f090Migration := func(actors *builtin.ActorTree) error {
+		f090ID, err := address.NewIDAddress(90)
+		if err != nil {
+			return xerrors.Errorf("failed to construct f090 id addr: %w", err)
+		}
+		f090OldAct, found, err := actorsOut.GetActorV5(f090ID)
+		if err != nil {
+			return xerrors.Errorf("failed to get old f090 actor: %w", err)
+		}
+		if !found {
+			return xerrors.Errorf("failed to find old f090 actor: %w", err)
+		}
+		f090NewSt := account14.State{Address: f090ID} // State points to ID addr
+		h, err := actors.Store.Put(ctx, &f090NewSt)
+		if err != nil {
+			return xerrors.Errorf("failed to write new f090 state: %w", err)
+		}
+
+		newAccountCodeCID, ok := newManifest.Get(manifest.AccountKey)
+		if !ok {
+			return xerrors.Errorf("invalid manifest missing account code cid")
+		}
+
+		return actorsOut.SetActorV5(f090ID, &builtin.ActorV5{
+			// unchanged
+			CallSeqNum: f090OldAct.CallSeqNum,
+			Balance:    f090OldAct.Balance,
+			Address:    f090OldAct.Address,
+
+			// changed
+			Code: newAccountCodeCID,
+			Head: h,
+		})
+	}
+	if err := f090Migration(actorsOut); err != nil {
+		return cid.Undef, err
 	}
 
 	outCid, err := actorsOut.Flush()
