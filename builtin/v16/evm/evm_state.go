e709224263c83bd0373cc3e7e1c89973ae7d7b0f
Mikers <snissn@gmail.com>
2025-01-29 16:12:30 -1000

    ai add godocs based on https://github.com/filecoin-project/builtin-actors/blob/5aad41bfa29d8eab78f91eb5c82a03466c6062d2/actors/evm/src/state.rs

--- builtin/v16/evm/evm_state.go
+++ builtin/v16/evm/evm_state.go
@@ -9,32 +9,46 @@ import (
 	"github.com/filecoin-project/go-state-types/builtin/v16/util/adt"
 )
 
-// TransientDataLifespan is a structure representing the transient data lifespan.
+// TransientDataLifespan represents the lifespan of transient data.
+// It includes an origin ActorID and a unique nonce to track the transaction.
 type TransientDataLifespan struct {
 	Origin abi.ActorID // The origin actor ID associated with the transient data.
-	Nonce  uint64 // A unique nonce identifying the transaction.
+	Nonce  uint64      // A unique nonce identifying the transaction.
 }
 
-// TransientData is a structure representing Transient Data
+// TransientData represents transient storage data in an EVM contract.
+// It consists of a state dictionary (KAMT<U256, U256>) and a lifespan tracker.
 type TransientData struct {
-	TransientDataState    cid.Cid // The contract transient data state dictionary. Transient Data State is a map of U256 -> U256 values. KAMT<U256, U256>
-	TransientDataLifespan TransientDataLifespan // The data representing the transient data lifespan
+	TransientDataState    cid.Cid               // CID of the transient data state dictionary.
+	TransientDataLifespan TransientDataLifespan // Data representing the transient data lifespan.
 }
 
+// Tombstone represents a marker for contracts that have been self-destructed.
 type Tombstone struct {
-	Origin abi.ActorID
-	Nonce  uint64
+	Origin abi.ActorID // The ActorID that initiated the self-destruct.
+	Nonce  uint64      // The transaction nonce at the time of self-destruction.
 }
 
+// State represents the on-chain state of an EVM contract.
+// It includes the contract bytecode, its hash, storage state, transient data, and nonce tracking.
 type State struct {
-	Bytecode      cid.Cid
-	BytecodeHash  [32]byte
-	ContractState cid.Cid
-	TransientData *TransientData
-	Nonce         uint64
-	Tombstone     *Tombstone
+	Bytecode      cid.Cid        // CID of the EVM contract bytecode.
+	BytecodeHash  [32]byte       // Keccak256 hash of the contract bytecode.
+	ContractState cid.Cid        // CID of the contract's persistent state dictionary.
+	TransientData *TransientData // Optional transient storage data associated with the contract.
+	Nonce         uint64         // Tracks how many times CREATE or CREATE2 have been invoked.
+	Tombstone     *Tombstone     // Optional marker indicating if the contract has been self-destructed.
 }
 
+// ConstructState initializes and returns a new contract state with an empty state dictionary.
+//
+// Parameters:
+//   - store: The ADT store used for state management.
+//   - bytecode: CID representing the contract bytecode.
+//
+// Returns:
+//   - *State: A pointer to the newly constructed State object.
+//   - error: An error if state initialization fails.
 func ConstructState(store adt.Store, bytecode cid.Cid) (*State, error) {
 	emptyMapCid, err := adt.StoreEmptyMap(store, builtin.DefaultHamtBitwidth)
 	if err != nil {
